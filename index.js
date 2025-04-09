016a0c3bf042fe765ba513bb5ed7caefa5177770
Pranto Deb <rajprit0m@outlook.com>
2025-04-09 23:15:23 +0600

    [DB] : [Connect MongoDB]

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
 dotenv.config();
-
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const uri = process.env.DB_URL;
 
 app.use(express.json());
 app.use(
@@ -15,6 +16,39 @@ app.use(
   })
 );
 
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    /**
+     * ************************************************************
+     *                       API ENDPOINT START
+     * ************************************************************
+     */
+
+    /**
+     * ************************************************************
+     *                       API ENDPOINT END
+     * ************************************************************
+     */
+  } finally {
+    await client.close();
+  }
+};
+
 app.get("/", (req, res) => {
   res.status(200).send("Running.........");
 });
@@ -22,3 +56,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
 });
+run().catch((error) => console.log(error));
