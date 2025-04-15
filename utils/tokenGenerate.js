7c50442dc14c32098d513daff0793284c1271434
Pranto Deb <rajprit0m@outlook.com>
2025-04-15 23:31:36 +0600

    [#]

--- utils/tokenGenerate.js
+++ utils/tokenGenerate.js
@@ -103,12 +103,5 @@ function generateToken(length = 32) {
 }
 
 const token = generateToken();
-console.log('Generated Token:', token);const crypto = require('crypto');
-
-function generateToken(length = 32) {
-  return crypto.randomBytes(length).toString('hex'); // 64 hex characters for 32 bytes
-}
-
-const token = generateToken();
 console.log('Generated Token:', token);
 console.log('Generated Token:', token);
