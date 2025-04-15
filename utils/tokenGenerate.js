ed998748e0baebc74002a67f1b2d440ff481bd19
Pranto Deb <rajprit0m@outlook.com>
2025-04-15 23:31:59 +0600

    [#]

--- utils/tokenGenerate.js
+++ utils/tokenGenerate.js
@@ -41,32 +41,4 @@ function generateToken(length = 32) {
 }
 
 const token = generateToken();
-console.log('Generated Token:', token);const crypto = require('crypto');
-
-function generateToken(length = 32) {
-  return crypto.randomBytes(length).toString('hex'); // 64 hex characters for 32 bytes
-}
-
-const token = generateToken();
-console.log('Generated Token:', token);const crypto = require('crypto');
-
-function generateToken(length = 32) {
-  return crypto.randomBytes(length).toString('hex'); // 64 hex characters for 32 bytes
-}
-
-const token = generateToken();
-console.log('Generated Token:', token);const crypto = require('crypto');
-
-function generateToken(length = 32) {
-  return crypto.randomBytes(length).toString('hex'); // 64 hex characters for 32 bytes
-}
-
-const token = generateToken();
-console.log('Generated Token:', token);const crypto = require('crypto');
-
-function generateToken(length = 32) {
-  return crypto.randomBytes(length).toString('hex'); // 64 hex characters for 32 bytes
-}
-
-const token = generateToken();
 console.log('Generated Token:', token);
