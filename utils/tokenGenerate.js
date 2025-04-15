7190abb78994f6c515c8aec1046eeae63b88f707
Pranto Deb <rajprit0m@outlook.com>
2025-04-15 23:30:43 +0600

    [#]

---
+++ utils/tokenGenerate.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+function generateToken(length = 32) {
+  return crypto.randomBytes(length).toString('hex'); // 64 hex characters for 32 bytes
+}
+
+const token = generateToken();
+console.log('Generated Token:', token);
