48bde116841fe04ed46af060b767140c70b00c21
Pranto Deb <rajprit0m@outlook.com>
2025-04-22 23:29:43 +0600

    [Auth Guard]

---
+++ middleware/authGuard.js
@@ -0,0 +1,11 @@
+const roleGuard = (requiredRole) => {
+    return (req, res, next) => {
+      if (!req.user || req.user.role !== requiredRole) {
+        return res.status(403).json({ message: 'Forbidden: Access denied' });
+      }
+      next();
+    };
+  };
+  
+  module.exports = roleGuard;
+  
