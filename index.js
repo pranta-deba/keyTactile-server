ac925e9924340014c322e183f046a3ccea4a0962
Pranto Deb <rajprit0m@outlook.com>
2025-04-27 02:45:19 +0600

    [Deployed Process].....

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(DB_URL, {
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173/"],
     credentials: true,
   })
 );
