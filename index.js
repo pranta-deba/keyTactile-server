3f5653d1089eff948f40da210e1e7bbfa302974a
Pranto Deb <rajprit0m@outlook.com>
2025-04-27 00:12:05 +0600

    [Deployed Process].....

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const auth = (req, res, next) => {
 
 const run = async () => {
   try {
-    // await client.connect();
+    await client.connect();
     // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
